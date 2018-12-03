469d40032684c4c6f65723a32939b2746bf8818a
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-03 11:12:23 +0545

    getting ready with data

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
-from flask import (
-Flask,
-render_template
-)
+from flask import render_template
+import connexion
 
 # create the application instance
-app = Flask(__name__,template_folder='templates')
+app = connexion.App(__name__,specification_dir='./')
 
+# Read the swagger.yml file to configure the endpoints
+app.add_api('swagger.yml')
+
+
+# Create a URL route in our application
 @app.route('/')
 def home():
     """
@@ -16,6 +19,7 @@ def home():
     """
     return render_template('home.html')
 
+
 # if we are running in the standalone mode, run the application
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
