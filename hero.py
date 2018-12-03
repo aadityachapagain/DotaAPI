3c78131d60980cac112b1e90f9fc62c2f5a04eee
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2018-12-03 13:48:36 +0545

    adding functionality of looking up sngle hero

--- hero.py
+++ hero.py
@@ -7,10 +7,16 @@ def get_data():
     return Heros
 
 # Create a handler for our get Heros
-def read():
+def read_all():
     """
      This function responds to a request for /api/hero
     with the complete lists of Heros
     :return: list of heros with their roles and counter
     """
     return get_data()
+
+def read_one(Heroname):
+    Heroname = Heroname.replace('_',' ').lower()
+    Heros = get_data()
+    hero = list(filter(lambda hero: hero["name"].lower() == Heroname,Heros))
+    return hero[0]
